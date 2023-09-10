bcb405d6f6cb7e7323767f8889a52dc9e39c360f
HolmedCarmenT <carmen.kaouk@holmedgroup.com>
2023-09-10 04:40:01 +0300

    ToDo

--- MontyTask.Data/Resources/AccessTokenResource.cs
+++
@@ -1,6 +0,0 @@
-namespace MontyTask.Data.Resources;
-public class AccessTokenResource
-{
-    public string AccessToken { get; set; } = string.Empty;
-    public long Expiration { get; set; }
-}
--- MontyTask.Data/Resources/UserResource.cs
+++
@@ -1,6 +0,0 @@
-namespace MontyTask.Data.Resources;
-public class UserResource
-{
-    public int Id { get; set; }
-    public string Email { get; set; } = string.Empty;
-}
